ab984bda9f1ba5a48239c6d9f559df7dcc98c009
Ashlyn Lewis <act56@byu.edu>
2019-11-09 12:54:19 -0700

    Saving changes.

---
+++ BlowOut-1/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using BlowOut_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut_1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View("Please call Support at <strong><u>801-555-1212.</u></strong> Thank you!");
+        }
+
+        public ActionResult Email(string name, string email)
+        {
+
+            return View("Index", "Thank you, " + name + "We will send an email to " + email);
+        }
+
+    }
+}
---
+++ BlowOut-1/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlowOut_1.Models
+{
+    public class Contact
+    {
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [StringLength(15)]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(1000)]
+        public string Message { get; set; }
+    }
+}
